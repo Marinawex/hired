8488af2be89598bd6d3014b8a2ac7e71d8e493cd
marinawex <sheep803@gmail.com>
2023-04-24 20:25:52 +0300

    zod validation for addPosition and Login forms

--- src/modals/AddPosition.tsx
+++ src/modals/AddPosition.tsx
@@ -4,42 +4,43 @@ import Modal from "react-bootstrap/Modal";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Feedback from "react-bootstrap/Feedback";
 import { useForm, SubmitHandler, Controller } from "react-hook-form";
-import {z} from 'zod'
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { fromZodError, ZodError } from "zod-validation-error";
 
-type Inputs = {
-  CompanyName: string;
-  Position: string;
-  date: string;
-  ContactName: string;
-  ContactEmail: string;
-  ContactPhoneNumber: Number;
-  save: string;
-};
+const AddPositionSchema = z
+  .object({
+    CompanyName: z.string().min(1, { message: "This field is required" }).max(40),
+    Position: z.string().min(2, { message: "This field is required" }).max(40),
+    ApplicationDate: z.string(),
+    ContactName: z.string().min(2).max(40),
+    ContactEmail: z.string().email(),
+    ContactPhoneNumber: z.string(),
+  })
+  .partial();
+
+type AddPositionSchema = z.infer<typeof AddPositionSchema>;
 
 function AddPosition() {
   const [show, setShow] = useState(false);
   const handleShow = () => setShow(true);
   const handleClose = () => setShow(false);
 
-  // const {
-  //   control,
-  //   handleSubmit,
-  //   watch,
-  //   formState: { errors },
-  // } = useForm<Inputs>();
-  // const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
-
   const {
     control,
     handleSubmit,
     reset,
     formState: { errors },
-    getValues,
-  } = useForm();
+  } = useForm<AddPositionSchema>({
+    resolver: zodResolver(AddPositionSchema),
+    mode: "onBlur",
+  });
 
-  const onSubmit = data => console.log(data);
-
+  const onSubmit = (data: AddPositionSchema) => {
+    console.log(data);
+  };
   return (
     <>
       <Button className="btn" variant="success" size="lg" onClick={handleShow}>
@@ -56,29 +57,34 @@ function AddPosition() {
                 <Form.Group className="mb-3" controlId="CompanyName">
                   <Form.Label>Company</Form.Label>
                   <Controller
+                    defaultValue=""
                     control={control}
                     name="CompanyName"
-                    defaultValue=""
-                    render={({ field: { onChange, onBlur, value, ref } }) => (
+                    render={({
+                      field: { onChange, onBlur, value, ref },
+                      fieldState: { invalid, isTouched, isDirty, error },
+                    }) => (
                       <Form.Control
                         onChange={onChange}
+                        onBlur={onBlur}
                         value={value}
                         ref={ref}
                         placeholder="Company name"
-                           
                       />
                     )}
                   />
-                       
+                  {errors.CompanyName?.message && (
+                    <p style={{color:'red'}}>{errors.CompanyName.message}</p>
+                  )}
                 </Form.Group>
               </Col>
               <Col>
                 <Form.Group className="mb-3" controlId="Position">
                   <Form.Label>Position</Form.Label>
                   <Controller
+                    defaultValue=""
                     control={control}
                     name="Position"
-                    defaultValue=""
                     render={({ field: { onChange, onBlur, value, ref } }) => (
                       <Form.Control
                         type="text"
@@ -90,15 +96,16 @@ function AddPosition() {
                       />
                     )}
                   />
+                  {errors.Position && <p style={{color:'red'}} >{errors.Position.message}</p>}
                 </Form.Group>
               </Col>
             </Row>
-            <Form.Group className="mb-3" controlId="Application date">
+            <Form.Group className="mb-3" controlId="ApplicationDate">
               <Form.Label>Application date</Form.Label>
               <Controller
-                control={control}
-                name="Application date"
                 defaultValue=""
+                control={control}
+                name="ApplicationDate"
                 render={({ field: { onChange, onBlur, value, ref } }) => (
                   <Form.Control
                     type="date"
@@ -109,6 +116,9 @@ function AddPosition() {
                   />
                 )}
               />
+              {errors.ApplicationDate && (
+                <p>{errors.ApplicationDate.message}</p>
+              )}
             </Form.Group>
             <h3>Contact Info</h3>
             <div
@@ -123,9 +133,9 @@ function AddPosition() {
                   <Form.Group className="mb-3" controlId="Contact Name">
                     <Form.Label>Contact Name</Form.Label>
                     <Controller
+                      defaultValue=""
                       control={control}
                       name="ContactName"
-                      defaultValue=""
                       render={({ field: { onChange, onBlur, value, ref } }) => (
                         <Form.Control
                           type="text"
@@ -137,15 +147,16 @@ function AddPosition() {
                         />
                       )}
                     />
+                    {errors.ContactName && <p>{errors.ContactName.message}</p>}
                   </Form.Group>
                 </Col>
                 <Col>
                   <Form.Group className="mb-3" controlId="Contact Email">
                     <Form.Label>Contact Email</Form.Label>
                     <Controller
+                      defaultValue=""
                       control={control}
                       name="ContactEmail"
-                      defaultValue=""
                       render={({ field: { onChange, onBlur, value, ref } }) => (
                         <Form.Control
                           type="text"
@@ -157,6 +168,9 @@ function AddPosition() {
                         />
                       )}
                     />
+                    {errors.ContactEmail && (
+                      <p style={{color:'red'}}>{errors.ContactEmail.message}</p>
+                    )}
                   </Form.Group>
                 </Col>
               </Row>
@@ -164,8 +178,8 @@ function AddPosition() {
                 <Form.Label>Phone</Form.Label>
                 <Controller
                   control={control}
-                  name="ContactPhone"
                   defaultValue=""
+                  name="ContactPhoneNumber"
                   render={({ field: { onChange, onBlur, value, ref } }) => (
                     <Form.Control
                       type="text"
@@ -177,19 +191,21 @@ function AddPosition() {
                     />
                   )}
                 />
+                {errors.ContactPhoneNumber && (
+                  <p>{errors.ContactPhoneNumber.message}</p>
+                )}
               </Form.Group>
             </div>
-            {/* <Controller control={control}                                                                
-          render={({ field: { ref }, formState }) => (                                               
-            <Button type="submit" disabled={formState.isSubmitting}                                  
-               className="btn btn-primary">                                                          
-               {formState.isSubmitting && <span className="spinner-border spinner-border-sm mr-1" />}
-               Save                                                                                  
-            </Button>                                                                                
-          )} />  */}
             <Modal.Footer>
-              <Button variant="success" onClick={handleClose} type="submit">
+              <Button variant="success" type="submit">
                 Add Position
+              </Button>
+              <Button
+                variant="success"
+                onClick={() => reset()}
+                value="Custom Reset Field Values & Errors"
+              >
+                reset
               </Button>
             </Modal.Footer>
           </Form>
--- src/modals/Login.tsx
+++ src/modals/Login.tsx
@@ -2,11 +2,45 @@ import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import Form from "react-bootstrap/Form";
+import { useForm, Controller } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const schema = z
+  .object({
+    email: z.string().email(),
+    password: z
+      .string()
+      .min(5, { message: "password must contain at least 5 characters" })
+      .max(20, { message: "password must contain at most 20 characters" }),
+    confirmPassword: z
+      .string()
+      .min(5, { message: "password must contain at least 5 characters" })
+      .max(20, { message: "password must contain at most 20 characters" }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  });
+
+type schema = z.infer<typeof schema>;
 
 const Login = () => {
   const [show, setShow] = useState(false);
   const handleShow = () => setShow(true);
   const handleClose = () => setShow(false);
+
+  const {
+    control,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<schema>({
+    resolver: zodResolver(schema),
+    mode: "onBlur",
+  });
+
+  const onSubmit = (data: schema) => console.log(data);
 
   return (
     <>
@@ -18,23 +52,85 @@ const Login = () => {
           <Modal.Title>Login</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form>
+          <Form onSubmit={handleSubmit(onSubmit)}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Email</Form.Label>
-              <Form.Control type="email" placeholder="Email" />
+              <Controller
+                defaultValue=""
+                control={control}
+                name="email"
+                render={({ field: { onChange, onBlur, value, ref } }) => (
+                  <Form.Control
+                    type="email"
+                    onChange={onChange}
+                    onBlur={onBlur}
+                    value={value}
+                    ref={ref}
+                    placeholder="Email"
+                  />
+                )}
+              />
+              {errors.email && (
+                <p style={{ color: "red" }}>{errors.email.message}</p>
+              )}
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="text" placeholder="Password" />
+              <Controller
+                defaultValue=""
+                control={control}
+                name="password"
+                render={({ field: { onChange, onBlur, value, ref } }) => (
+                  <Form.Control
+                    type="password"
+                    onChange={onChange}
+                    onBlur={onBlur}
+                    value={value}
+                    ref={ref}
+                    placeholder="Password"
+                  />
+                )}
+              />
+              {errors.password && (
+                <p style={{ color: "red" }}>{errors.password.message}</p>
+              )}
             </Form.Group>
+            <Form.Group className="mb-3" controlId="ConfirmformBasicPassword">
+              <Form.Label> Confirm Password</Form.Label>
+              <Controller
+                defaultValue=""
+                control={control}
+                name="confirmPassword"
+                render={({ field: { onChange, onBlur, value, ref } }) => (
+                  <Form.Control
+                    type="password"
+                    onChange={onChange}
+                    onBlur={onBlur}
+                    value={value}
+                    ref={ref}
+                    placeholder="Confirm password"
+                  />
+                )}
+              />
+              {errors.confirmPassword && (
+                <p style={{ color: "red" }}>{errors.confirmPassword.message}</p>
+              )}
+            </Form.Group>
+            <Modal.Footer>
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+              <Button
+                variant="primary"
+                onClick={() => reset()}
+                value="Custom Reset Field Values & Errors"
+              >
+                reset
+              </Button>
+            </Modal.Footer>
           </Form>
         </Modal.Body>
-        <Modal.Footer>
-          <Button variant="primary" type="submit" onClick={handleClose}>
-            Submit
-          </Button>
-        </Modal.Footer>
       </Modal>
     </>
   );
